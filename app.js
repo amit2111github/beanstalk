c6bed02accaafb19b7d9265598df313d380bd337
amit2111github <amitkumarmandal2111@gmail.com>
2022-12-13 13:23:11 +0530

    added a html page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const app = require("express")();
-
+const path = require("path");
 app.get("/" , (req , res) => {
 	return res.send("Your are on the Home page");
+});
+app.get("/home" , (req , res) =>{
+	console.log(path.join(__dirname , "/"));
+	res.sendFile(path.join(__dirname , "index.html" ));
 });
 app.get("/:name" , (req , res) => {
 	const {name} = req.params;
